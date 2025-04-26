fec232f26a63ba19ebfcd48264533d92d1346437
Esce <p.demski100@gmail.com>
2025-04-26 13:59:00 +0200

    Updated utils

--- app/utils.py
+++ app/utils.py
@@ -3,11 +3,16 @@ from app.models import RatingCriteria, CriteriaPageNumber
 
 
 def get_form_class(criteria):
-    if criteria == RatingCriteria.PAGE_NUMBER:
-        return CriteriaPageNumberForm
-    return None
+    match criteria:
+        case RatingCriteria.PAGE_NUMBER:
+            return CriteriaPageNumberForm
+        case _:
+            return None
+
 
 def get_criteria_model(criteria):
-    if criteria == RatingCriteria.PAGE_NUMBER:
-        return CriteriaPageNumber
-    return None
+    match criteria:
+        case RatingCriteria.PAGE_NUMBER:
+            return CriteriaPageNumber
+        case _:
+            return None
