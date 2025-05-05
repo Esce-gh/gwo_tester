524a19799afdcf7799d15dda0689e2e3e7c3a174
Esce <p.demski100@gmail.com>
2025-05-05 13:54:33 +0200

    Changed nginx body size limit and added registered fields in admin

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from app.models import Service, Rating, Page, CriteriaPageNumber
+from app.models import Service, Rating, Page, CriteriaPageNumber, CriteriaHeaderFooter, CriteriaObjectDetection
 
 admin.site.register(Service)
 admin.site.register(Page)
 admin.site.register(Rating)
 admin.site.register(CriteriaPageNumber)
+admin.site.register(CriteriaHeaderFooter)
+admin.site.register(CriteriaObjectDetection)
